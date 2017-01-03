0b2ffd41cac19dd7fcf2a07fdbf42177b559df6a
ejchoi@apexsoft.co.kr <ejchoi@apexsoft.co.kr>
2017-01-03 21:20:49 +0900

    c d로 변경

--- src/main/java/day2/Algebra.java
+++ src/main/java/day2/Algebra.java
@@ -4,7 +4,7 @@ package day2;
  * Created by 최은주 on 2017-01-03.
  */
 public class Algebra {
-    public int subtract (int a, int b) {
-        return a-b;
+    public int subtract (int c, int d) {
+        return c-d;
     }
 }

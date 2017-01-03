145494845747f12bc281987b6219639e63027cb8
ejchoi@apexsoft.co.kr <ejchoi@apexsoft.co.kr>
2017-01-03 20:56:09 +0900

    뺄셈 추가

---
+++ src/main/java/day2/Algebra.java
@@ -0,0 +1,10 @@
+package day2;
+
+/**
+ * Created by 최은주 on 2017-01-03.
+ */
+public class Algebra {
+    public int subtract (int a, int b) {
+        return a-b;
+    }
+}
---
+++ src/test/java/day2/AlgebraTest.java
@@ -0,0 +1,17 @@
+package day2;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by 최은주 on 2017-01-03.
+ */
+public class AlgebraTest {
+    @Test public void  subtractTest() {
+        Algebra algebra = new Algebra();
+        assertThat("3-1=2", algebra.subtract(3, 1), is(equalTo(2)));
+    }
+}
